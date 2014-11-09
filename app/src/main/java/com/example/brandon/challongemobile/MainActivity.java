1f228ac48fde2c05bc65d9671e97469258b95646
Brandon <brandonrninefive@gmail.com>
2014-11-08 23:07:16 -0500

    <stay logged in> functions all work

--- app/src/main/java/com/example/brandon/challongemobile/MainActivity.java
+++ app/src/main/java/com/example/brandon/challongemobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.brandon.challongemobile;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -13,10 +14,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.CheckBox;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.net.Authenticator;
 import javax.net.ssl.HttpsURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URL;
+import java.util.Scanner;
 
 
 public class MainActivity extends ActionBarActivity
@@ -25,7 +34,6 @@ public class MainActivity extends ActionBarActivity
     EditText usernameView;
     EditText passwordView;
     CheckBox checkB;
-
 
 
     @Override
@@ -39,35 +47,43 @@ public class MainActivity extends ActionBarActivity
         passwordView = (EditText) findViewById(R.id.passwordTextField);
         checkB = (CheckBox) findViewById(R.id.checkBox);
 
+        System.out.println("on create");
+
+        String content;
+        BufferedReader in;
+        try {
+            File file = new File(getFilesDir(),"userCred.txt");
+            Scanner scan = new Scanner(file);
+            if(file.exists())
+            {
+                //System.out.println("file exists");
+                URL url = new URL("https://api.challonge.com/v1/tournaments.xml?state=in_progress");
+
+                HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+
+                connection.setRequestProperty("Accept-Encoding","");
+                //connection.setRequestProperty("Authorization","Basic " + new String(Base64.encode("bubblerugs:AJmK8DFMF0EpwVRzTlORtuwyJOcGzViDXrQKG63G".getBytes(),Base64.NO_WRAP)));
+                connection.setRequestProperty("Authorization","Basic " + new String(Base64.encode(new String(scan.nextLine() + ":" +scan.nextLine()).getBytes(),Base64.NO_WRAP)));
+
+                handleSuccess("","");
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("File Not Found");
+        }
+
 
     }
 
             public void onClick(View v)
             {
-
-
-                //errorView.setVisibility(View.INVISIBLE);
-
-                /*
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
-                */
-
-                //Toast toast = Toast.makeText(context, text, duration);
-
                 new Thread(new Runnable()
                 {
                     public void run()
                     {
                         try
                         {
-
-                            if(checkB.isChecked())
-                            {
-                                System.out.println("box is checked");
-
-                            }
 
                             final String username = usernameView.getText().toString();
                             final String password = passwordView.getText().toString();
@@ -77,26 +93,25 @@ public class MainActivity extends ActionBarActivity
                             HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
 
                             connection.setRequestProperty("Accept-Encoding","");
-                            //connection.setRequestProperty("Authorization","Basic " + new String(Base64.encode("bubblerugs:AJmK8DFMF0EpwVRzTlORtuwyJOcGzViDXrQKG63G".getBytes(),Base64.NO_WRAP)));
-                            connection.setRequestProperty("Authorization","Basic " + new String(Base64.encode(new String(username + ":" +password).getBytes(),Base64.NO_WRAP)));
+                            connection.setRequestProperty("Authorization","Basic " + new String(Base64.encode("bubblerugs:AJmK8DFMF0EpwVRzTlORtuwyJOcGzViDXrQKG63G".getBytes(),Base64.NO_WRAP)));
+                            //connection.setRequestProperty("Authorization","Basic " + new String(Base64.encode(new String(username + ":" +password).getBytes(),Base64.NO_WRAP)));
 
                             connection.connect();
+
                             if(connection.getResponseCode() == 200)
                             {
-                                handleSuccess();
+                                handleSuccess(username,password);
                             }
                             else
                             {
-                                MainActivity.this.runOnUiThread(new Runnable() {
-                                    public void run() {
-                                        Toast.makeText(getApplicationContext(), "Incorrect Login Credentials", Toast.LENGTH_SHORT).show();
+                                MainActivity.this.runOnUiThread(new Runnable()
+                                {
+                                    public void run()
+                                    {
+                                        Toast.makeText(getApplicationContext(), "Incorrect Login Credentials", Toast.LENGTH_LONG).show();
+                                       // System.out.println("Error response code: " + connection.getResponseCode());
                                     }
                                 });
-
-                                //System.out.println(connection.getResponseCode() + ": " +connection.getResponseMessage());
-                                System.out.println("toasting");
-                                //add toast
-                               // if(connection.getResponseCode() == )
                             }
                         }
                         catch (Exception e)
@@ -109,21 +124,17 @@ public class MainActivity extends ActionBarActivity
 
 
 
-    public void makeToast()
-    {
-        Toast.makeText(getApplicationContext(), "Incorrect Login Credentials", Toast.LENGTH_SHORT).show();
-    }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -132,8 +143,27 @@ public class MainActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
-    private void handleSuccess()
+    private void handleSuccess(String username, String password)
     {
+        System.out.println("Success");
+
+        if(checkB.isChecked())
+        {
+
+            try {
+                FileOutputStream fos = openFileOutput("userCred.txt",Context.MODE_PRIVATE);
+                //fos.write((username+"\n"+password).getBytes());
+                fos.write(("bubblerugs"+"\n"+"AJmK8DFMF0EpwVRzTlORtuwyJOcGzViDXrQKG63G").getBytes());
+
+                fos.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+
         Intent intent = new Intent(this,ChallongeHome.class);
         startActivity(intent);
         finish();
